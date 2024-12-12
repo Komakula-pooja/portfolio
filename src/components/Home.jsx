c361b386f664e4eda2cb43343884d56904e41f53
Komakula-pooja <poojithakomakula21@gmail.com>
2024-12-12 13:51:17 +0530

    added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
-import Logo from '../assets/logo.png';
 import { Link } from 'react-scroll';
 
 const Home = () => {
