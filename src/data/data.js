67c72051ba5148d02acaa5e927b135987515c2a7
Komakula-pooja <poojithakomakula21@gmail.com>
2025-01-06 12:35:32 +0530

    added

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,6 @@
 
 import medium from '../assets/mediumpage.png'
+import saptjanm from '../assets/saptjanm.png'
 
 export const data=[
     {
@@ -9,4 +10,11 @@ export const data=[
         github:"https://github.com/Komakula-pooja/frontend",
         live:"https://medium-komakula.vercel.app/",
     },
+    {
+        id:2,
+        name:"Fullstack - SaptJanm Web Application",
+        image:saptjanm,
+        github:"https://github.com/Komakula-pooja/SaptJanm-frontend",
+        live:"https://sapt-janm.vercel.app/",
+    }
 ]
