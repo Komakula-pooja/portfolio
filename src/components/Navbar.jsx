dd85bd150da3732190b366ac95eb71c47b718a24
Komakula-pooja <poojithakomakula21@gmail.com>
2025-01-06 12:39:55 +0530

    added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -107,7 +107,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1pL64HmrVgY6kusWwFBu7qv1AUN4JCFur/view?usp=drivesdk' target='_blank'
+              href='https://drive.google.com/file/d/11Q-pP-kud8TbkubBz0Uildi5_bxYoSh0/view?usp=drivesdk' target='_blank'
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
